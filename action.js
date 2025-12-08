49958d51dc8a8999301939001796bdae8bbe02d0
link-short <dora.family228@gmail.com>
2025-12-08 17:21:16 +0500

    Update action.js

--- action.js
+++ action.js
@@ -1,13 +1,20 @@
 const urlParams = new URLSearchParams(window.location.search);
 const type = urlParams.get('type');
 const fileName = urlParams.get('file');
+const visibleAlert = urlParams.get('alert');
 console.log(type);
 console.log(fileName);
 
 if (type == "cookieDialog") {
   if (fileName == "winnicookies.cookie") {
-    alert(type);
-    alert(fileName);
+    if (visibleAlert == "true") {
+      alert(type);
+      alert(fileName);
+    }
+    else {
+       console.log(type);
+      console.log(fileName);
+    }
     console.log("Cookie Visible Now!!!");
     console.log(type);
     console.log(fileName);
