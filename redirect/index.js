1df34b3eb7646ecff7b8f95f5aa3efd551cb18d8
link-short <dora.family228@gmail.com>
2025-12-08 19:22:48 +0500

    Update index.js

--- redirect/index.js
+++ redirect/index.js
@@ -7,14 +7,14 @@ async function openUrlSiteOrPage(urlToRedirect) {
   window.location.href = urlToRedirect;
 }
 
-if (url_end == "?from=Winnigames2024-Original | Redirecter") {
+if (url_end == "from=Winnigames2024-Original%20|%20Redirecter") {
   params.append('url', url);
-  params.append('url_end', '?from=Winnigames2024+Service.Redirecter');
+  params.append('url_end', 'from=Winnigames2024+Service.Redirecter');
   const url4343 = 'https://winnigames2024-original.github.io/action/redirect?' + params.toString();
   openUrlSiteOrPage(url4343);
 }
 else {
-  const full_url = url + url_end;
+  const full_url = url + "?" + url_end;
   openUrlSiteOrPage(full_url);
 }
 // https://winnigames2024-original.github.io/action/?type=redirect&url=https://example.com
