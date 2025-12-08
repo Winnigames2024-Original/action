ef2ad7999386a7a0ff19249947b7627cc15fa064
link-short <dora.family228@gmail.com>
2025-12-08 22:58:38 +0500

    Update index.js

--- redirect/index.js
+++ redirect/index.js
@@ -1,13 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 const url = urlParams.get('url');
 
-async function openUrlSiteOrPage(urlToRedirect) {
-  await delay(3000); // Wait for 3 seconds
-  window.location.href = urlToRedirect;
-}
+setTimeout(() => {
+  window.location.href = url;
+}, 2000); // 2000 milliseconds = 2 seconds
 
 
-openUrlSiteOrPage(url);
 
 // https://winnigames2024-original.github.io/action/?type=redirect&url=https://example.com
 
