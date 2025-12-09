3a26e5b4edf4e58281d9b5e1ed74359662808784
link-short <dora.family228@gmail.com>
2025-12-09 15:20:15 +0500

    Update index.js

--- download.winnilang/index.js
+++ download.winnilang/index.js
@@ -1,6 +1,13 @@
 const urlParams = new URLSearchParams(window.location.search);
 const urlDsd = urlParams.get('url');
 
+const button = document.getElementById('downloadFileButton');
+
+button.addEventListener('click', function() {
+  console.log("Кнопка нажата через addEventListener!");
+  document.body.style.backgroundColor = 'lightblue';
+  window.location.href = urlDsd;
+});
 function downloadButtonClick() {
   window.location.href = urlDsd;
 }
